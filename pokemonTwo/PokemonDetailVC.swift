5299a5a4da3d831a635093ac6a99bac3a672b91e
Chris Hovey <chrishovey@Chriss-MacBook-Air.local>
2016-06-03 13:03:13 -0600

    about to add cocoa pods

--- pokemonTwo/PokemonDetailVC.swift
+++ pokemonTwo/PokemonDetailVC.swift
@@ -12,10 +12,27 @@ class PokemonDetailVC: UIViewController {
 
     var pokemon: Pokemon!
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var pokeDesc: UILabel!
+    @IBOutlet weak var type: UILabel!
+    @IBOutlet weak var defense: UILabel!
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var pokedexId: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLbl.text = pokemon.name
     }
 
+    
+    @IBAction func backBtnPress(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
